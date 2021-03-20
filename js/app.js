0c2d511d8582101eb2dda4a6b200b5daee6d99af
testings-app <81051430+testings-app@users.noreply.github.com>
2021-03-20 19:27:02 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const audio = document.querySelector("audio")
+audio.autoplay = true;
 let sonid = document.querySelector('#sonic');
 let i = document.querySelector('i');
 sonic.children[0].addEventListener('click', () => {
