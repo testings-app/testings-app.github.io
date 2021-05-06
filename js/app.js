2f3ead5b55e3a5e59b7e453d58457750e88ef6a3
testings-app <81051430+testings-app@users.noreply.github.com>
2021-05-06 17:11:55 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,1 @@
-const audio = document.querySelector("audio")
-let sonid = document.querySelector('#sonic');
-let i = document.querySelector('i');
-sonic.children[0].addEventListener('click', () => {
-    if (audio.muted == true) {
-        audio.muted = false;
-        i.classList.replace("fa-volume-mute", "fa-volume-off");
-    } else {
-        audio.muted = true;
-        i.classList.replace("fa-volume-off", "fa-volume-mute");
-    }
-});
+
